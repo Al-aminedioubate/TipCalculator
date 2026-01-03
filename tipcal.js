ceaa14988de4309ee19eb91df975cada0af8a91c
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-03 18:45:32 -0500

    calcul de tip terminer

--- tipcal.js
+++ tipcal.js
@@ -1,6 +1,24 @@
 //Declaration et initialisation de nos variables
 let bAmount = document.getElementById("bill_amt");
-let tpercentage = document.getElementById("tip_per");
+let tPercentage = document.getElementById("tip_per");
 
 let btnTotal = document.getElementById("btn_cal");
 let totalRes = document.getElementById("totalAmount");
+
+//Fonction de traitement de calcul de pourcentage
+function amountTotal(amnt1, amnt2) {
+	let percentage = (amnt1 * amnt2) / 100;
+	let total = amnt1 + percentage;
+
+	return total;
+}
+
+//mettons notre fonction dans une variable pour facilité l'utilisation
+btnTotal.addEventListener("click", () => {
+	let val1 = Number(bAmount.value);
+	let val2 = Number(tPercentage.value);
+
+	let result = amountTotal(val1, val2);
+
+	totalRes.textContent = `Total : $` + `${result}.00 `;
+});
