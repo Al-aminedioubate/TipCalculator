0b75d109559840e0de71577f1c2960bcd5cef2b2
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-03 17:14:34 -0500

    declaration des variables terminer

---
+++ tipcal.js
@@ -0,0 +1,6 @@
+//Declaration et initialisation de nos variables
+let bAmount = document.getElementById("bill_amt");
+let tpercentage = document.getElementById("tip_per");
+
+let btnTotal = document.getElementById("btn_cal");
+let totalRes = document.getElementById("totalAmount");
